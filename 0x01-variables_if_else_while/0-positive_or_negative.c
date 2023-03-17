a998d7ba2251b8f8648aefee83e78e8ac9b4987b
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-17 09:22:30 -0700

    Positive negative zero numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/* print whether the number is negative or positive */
+
 int main(void)
 {
 	int n;
@@ -10,5 +13,27 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	/* ask user to enter number */
+	
+	printf("Enter a number: \n");
+	/* prompt user for number */
+	
+	scanf("%d", n);
+		printf("User entered %d as input\n", n);
+	
+	/* print type of number entered by user */	
+	
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else 
+	{
+		printf("%d is zero\n", n);
+	}	
 	return (0);
 }
