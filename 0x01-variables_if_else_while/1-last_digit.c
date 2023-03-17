9aa54e21d9bf31573f774cf81a610dd4cba8ea2c
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-17 13:02:44 -0700

    Random checker

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	if (n%10 > 5)
 	{
 		int rem = n%10;
-		printf("Last digit of %d is and %d is greater than 5\n", n, rem);
+		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
 	}
 	if (n%10 == 0)
 	{
