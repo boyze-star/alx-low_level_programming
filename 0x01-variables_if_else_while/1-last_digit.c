c7728e1416e1e34349612393a2d5648c93124296
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-17 11:25:31 -0700

    My random assined number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/*  */
+/* main - rogram will assign a random number to the variable n each time it is executed */
+/*Return: Always */
+
 int main(void)
 {
 	int n;
@@ -11,5 +13,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	if (n > 5)
+	{
+		printf("Last digit of %d  is greater than 5\n", n);
+	}
+	if (n == 0)
+	{
+		printf("Last digit of %d is 0\n", n);
+	}
+	if (n < 6 && n != 0)
+	{
+		printf("Last digit of %d is less than 6 and not 0\n", n);
+	}
 	return (0);
 }
