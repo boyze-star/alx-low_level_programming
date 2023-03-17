927b6bbaa6d69fc1ca00ebb2a897a3653e1c78af
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-17 13:09:39 -0700

    Random checker

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* main - rogram will assign a random number to the variable n each time it is executed */
-/*Return: Always */
+/** main - rogram will assign a random 
+ * number to the variable n each time it is executed */
+/*Return: Always 0 */
 
 int main(void)
 {
@@ -16,15 +17,17 @@ int main(void)
 	if (n % 10 > 5)
 	{
 		int rem = n % 10;
+
 		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
 	}
 	if (n % 10 == 0)
 	{
 		printf("Last digit of %d is 0 and is 0\n", n);
 	}
-	if (n % 10 < 6 && n%10 != 0)
+	if (n % 10 < 6 && n % 10 != 0)
 	{
 		int rem2 = n % 10;
+
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rem2);
 	}
 	return (0);
