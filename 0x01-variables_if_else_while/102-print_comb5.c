d40b54690f691bf29febe0b41b706fa97fff91d9
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-17 20:19:19 -0700

    Posssible combinations of two numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main (void)
+{
+	char a, b;
+
+	for (a = 0;a < 100;a++)
+	{
+		for (b = 0;b < 100;b++)
+		{
+			if (a < b)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(" ");
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 99 || b != 99)
+				{
+					putchar(" , ");
+					putchar(" ");
+				}
+			}		
+		}			
+	}
+	putchar("\n");
+	return(0);
+}	
