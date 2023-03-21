203c0864de1f8325ec849218ad07bd5a071ba5ad
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-21 13:23:05 -0700

    Printing characters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,18 +7,7 @@
  */
 int main(void)
 {
-	/* description body
-	 * using 'write' to print
-	 * open file as '.c' extension
-	 * execute main() function
-	 * write() is a system call
-	 * takes three  arguements as file descriptor
-	 * first constant string
-	 * second size of constant string
-	 * including '\O'
-	 * compile as cc file_name
-	 * execute as ./a.out
-	 * include <unistd.h>
+	/* printing a single string
 	 */
 	write(1, "_putchar\n", 9);
 	return (0);
