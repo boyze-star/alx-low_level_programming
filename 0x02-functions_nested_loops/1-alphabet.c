1c2bc8fd206c0f418a848886123da7aa81ff2adc
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-20 17:07:15 -0700

    Printing single characters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/* defins tolower() function 
+ * converts uppercase alphabet to lower case
+ * returns same passed character
+ * if its anything either than
+ */
+/* character is stored in int form
+ * when character is passed as arguement
+ * corresponding ASCII value(int)
+ * is passed instead of 
+ * character itself
+ */
+int main()
+{
+	int ch; 
+
+	while(1)
+	{
+		ch = getchar();
+		putchar(ch);
+	}
+	return(0);
+
+}	
