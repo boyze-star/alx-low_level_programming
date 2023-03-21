3e9b7688cf209ffe8f0c50421037141d35416c4c
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-21 01:17:06 -0700

    Print without printf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,15 +10,13 @@ int main(void)
 	 */
 	/* using 'write' to print
 	 * open file as '.c' extension
-	 */
-	/* execute main() function
+	 * execute main() function
 	 * write() is a system call
 	 * takes three  arguements as file descriptor
 	 * first constant string
 	 * second size of constant string
 	 * including '\O'
-	 */
-	/* compile as cc file_name
+	 * compile as cc file_name
 	 * execute as ./a.out
 	 * include <unistd.h>
 	 */
