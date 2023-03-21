d8a86fc9a7bb9172a404064c5e9c39563475ada6
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-21 03:02:19 -0700

    Function printing lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,22 +5,17 @@
  * converts uppercase alphabet to lower case
  * returns same passed character
  * if its anything either than
- */
-/* character is stored in int form
+ * character is stored in int form
  * when character is passed as arguement
  * corresponding ASCII value(int)
  * is passed instead of 
- * character itself
+ * character
  */
-void main(void)
+int main(void)
 {
-	char  ch; 
-
-	while(1)
-	{
-		ch = getchar();
-		putchar(ch);
-	}
-	return(0);
-
+	char ch = tolower("A-Z, a-z");
+	scanf("%c", ch);
+	fwrite(1, "ch\n", 3);
+	return (0);
 }	
+	
