ab302557faeddc882cf007a56d8b1b8290e11894
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-21 19:56:54 -0700

    Lowecase alphabets

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-/* lowercase chars
- * @c: check lowercase chars
- * Return 1 if char is c
- * and 0 if anything else
+/*lowercase chars
+ *@c: check lowercase chars
+ *Return 1 if char is c
+ *and 0 if anything else
  */
 int _islower(int c)
 {
