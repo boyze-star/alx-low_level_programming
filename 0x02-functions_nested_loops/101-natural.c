c0145596a3185aa2ad93b97c084cfbfdd8eafd70
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-22 09:27:39 -0700

    Natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h."
+#include <stdio.h>
+
+/* Listing natural numbers */
+int main(void)
+{
+	int x;
+	sum = 0;
+
+	for (x = 0;x < 1024;x++)
+	{
+		if ((x % 3) == 0 || (x % 5) == 0)
+		{
+			sum = sum + x
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}
