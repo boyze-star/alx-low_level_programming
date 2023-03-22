df3c63472674fa12478368d9f8fd669a0913eb66
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-21 18:41:56 -0700

    Print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+	/* print all alphabet in lower case
+	 */
+	void print_alphabet(void)
+	{
+		char alphabet;
+		for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+		_putchar('\n');
+	}
