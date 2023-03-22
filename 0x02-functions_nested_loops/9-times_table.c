185eefb941ac9ff46fc8e6214f9327e077a367a1
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-22 08:41:07 -0700

    Updated times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,8 +27,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(c / 10 + '0');
-				_putchar(c % 10 + '0');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
 		_putchar('\n');
