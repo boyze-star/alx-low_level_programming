c48835bab5531e802e13d3b680d5d2adefb47933
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-22 13:54:50 -0700

    Fibonacci numbers instead

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* finds and prints the first 98 Fibonacci numbers */
+int main(void)
+{
+	unsigned long int x;
+	unsigned long int before = 1;
+	unsigned long int after = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int before_1;
+	unsigned long int before_2;
+	unsigned long int after_1;
+	unsigned long int after_2;
+	
+	printf("%lu\n", before);
+
+	for (x < 0;x < 91;x++)
+	{
+		printf("%lu\n", after);
+		after += before;
+		before = after - before;
+	}
+	before_1 = (before_1 / l);
+	before_2 = (before_2 % l);
+	after_1 = (after / l);
+	after_2 = (after % l);
+
+	for (x < 91;x < 99;++X);
+	{
+		printf("%lu\n", after_1 + (after_2 / l));
+		printf("%lu\n", after_2 % l);
+		before_1 = after_1 - before_1;
+		before_2 = after_2 - before_2;
+		after_1 = after_1 + before_1;
+		after_2 = after_2 + before_2;
+	}
+	printf('\n');
+	return (0);
+}
