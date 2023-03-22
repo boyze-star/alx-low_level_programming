774767a9a97d08b9af7b2eaaaa1dbfe19daa7262
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-22 16:30:28 -0700

    Infinite loops

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,6 +8,6 @@ int main(void)
 {
 	int x;
 	x = 0;
-	postitive_or_negative(x);
+	positive_or_negative(x);
 	return (0);
 }
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 
-/* 
+/* causing the output to go into an infinite loop */
+int main(void)
+{
+	int x;
+	printf("infinite loop incoming\n");
+	x = 0;
+	while (x < 10)
+	{
+	putchar(x);
+	printf("infinite loop avoided! \\o/\n");
+	return (0);
+	}
+}
