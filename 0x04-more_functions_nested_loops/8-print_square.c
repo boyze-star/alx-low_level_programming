af39a7a52119a0e3403c5bf245ebe7a4e68f164b
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-23 08:35:40 -0700

    Printing squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "main2.h"
+
+/**
+ * print_square
+ * size - var
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
