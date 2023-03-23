4a16e7c6a7c7a9a5e93fd84d119d6eb1ad05172d
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-23 05:40:21 -0700

    Checking for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if value is digit
+ * c is var
+ * return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (0);
+	}
+	return (0);
+}
