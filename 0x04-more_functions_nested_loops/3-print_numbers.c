370ed02607ccab479dc15a02b78d41b6e74a131c
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-23 06:21:34 -0700

    Printing out numbers 48-57

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "main2.h"
+
+/**
+ * print_numbers - printing numbers rom 0 - 9
+ * @void - no var
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(i);
+}
