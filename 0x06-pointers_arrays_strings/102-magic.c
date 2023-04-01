c3c0bbe67a1ae22ed39439f43dacb205a0770583
Thlakudi <thlakudimsibi760@gmail.com>
2023-03-31 17:02:38 -0700

    Adding magic lines

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * Adds one line to my code
+ * @a: variable
+ * @n: variable
+ * @p: pointer
+ * Return: Always 0
+ */
 int main(void)
 {
   int n;
@@ -8,16 +14,8 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
+
   *(p + 5) = 98;
-  /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
