eb93ad6b4b2da6a45705dfaf424970d2e39d6f1d
Thlakudi <thlakudimsibi760@gmail.com>
2023-04-03 19:59:12 -0700

    function that sets the value of a pointer to a char

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,22 +2,20 @@
 #include "_putchar.c"
 
 /**
- * print_chessboard - function that prints a chessboard
- * @a: array of two dimension
+ * print_chessboard - function that prints chessboard
+ * @a: 2d array
  *
  * Return: Always 0.
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j =0; j < 8; j++)
-		{
-			_putchar([i][j]);
-			_putchar('\n');
+		for (j = 0; j < 8; j++)
+		{	_putchar(a[i][j]);
+		_putchar('\n');
 		}
 	}
 }
