b26e851807f92cf66c6ec661ab4ef294c59685ce
Thlakudi <thlakudimsibi760@gmail.com>
2023-04-04 01:37:35 -0700

    function copies memory from srcto dest

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * _memcpy - function that copies memory area.
  * @dest: destinaton variable
  * @src: source variable
  * @n: counter variable
- *
- * Returns: Always 0.
+ * Return: Always 0.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
