7c950c9e3b03273c76bb7469e63ac64573e1a319
Thlakudi <thlakudimsibi760@gmail.com>
2023-04-10 01:59:59 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * programme print name
+ * @void: - function not passed any parameters
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Programme name : %s\n", 0-whatsmyname.c);
+	return (0);
+}
