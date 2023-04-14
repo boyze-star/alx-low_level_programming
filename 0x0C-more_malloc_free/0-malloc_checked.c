dbd2a37acdd3eca38cd6b045fabce8d7385d0b9f
Thlakudi <thlakudimsibi760@gmail.com>
2023-04-14 14:31:00 -0700

    function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,7 +11,17 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = (void *)malloc(b * sizeof(unsigned int));
+	/**
+	 * void *ptr = (void *)malloc(b * sizeof(unsigned int));
+	 */
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit (98);
+	}
 
 	return (ptr);
 }
