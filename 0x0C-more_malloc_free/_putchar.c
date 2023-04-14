a297989b5791416ff76b3a2510b5483191df2bd0
Thlakudi <thlakudimsibi760@gmail.com>
2023-04-14 16:35:21 -0700

    Update

---
+++ 0x0C-more_malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to print
+ *
+ * Return: Always 0(Success), 1(Error).
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
