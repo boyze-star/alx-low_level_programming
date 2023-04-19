5209df9be456a90b0fd97c24a9908d1c3d380b0f
Thlakudi <thlakudimsibi760@gmail.com>
2023-04-19 14:43:30 -0700

    executes function given as parameter

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,24 @@
-#include 
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - prints array elements each on newline
+ * @array: array
+ * @size: how many elements to print
+ * @action: pointer to print in regular of hex
+ *
+ * Return: Void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
