781becedce4234f47aaca6701836cb2f963c644a
Thlakudi <thlakudimsibi760@gmail.com>
2023-04-21 14:03:01 -0700

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
