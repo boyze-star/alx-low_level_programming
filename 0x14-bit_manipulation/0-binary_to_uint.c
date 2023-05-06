689c3994aa331b5ba45ca8f4a46c05e984851890
Thlakudi <thlakudimsibi760@gmail.com>
2023-05-06 16:47:14 -0700

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -23,11 +23,11 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; i < len; i++)
 	{
-		if (b[i]  <= '0')
+		if (b[i]  == '0')
 		{
 			result = result << 1;
 		}
-		else if (b[i] >= '1')
+		else if (b[i] == '1')
 		{
 			result = result << 1 | 0x01;
 		}
