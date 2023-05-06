0e00334565695c076448d5ad2668518183fdf08c
Thlakudi <thlakudimsibi760@gmail.com>
2023-05-06 16:38:01 -0700

    binary to uint

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - function prints character
+ * @c: character to be printed
+ *
+ * Return: 0(Success), 1(Error)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
