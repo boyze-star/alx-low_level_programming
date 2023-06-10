0f2e9f0339ab64e8005f61bef6c7c664177a2918
Thlakudi <thlakudimsibi760@gmail.com>
2023-06-09 19:48:38 -0700

    number of arguements

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,8 +3,8 @@
 
 /**
  * main - print the name of the program
- * argc: Count arguments
- * argv: Arguments
+ * @argc: Count arguments
+ * @argv: Arguments
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
