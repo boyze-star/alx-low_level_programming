58e0f89889af2bb6ae1cb7b29fbc0a4eeac4869a
Thlakudi <thlakudimsibi760@gmail.com>
2023-06-13 13:52:02 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * ceate_array - function creates array and replaces intialised
+ * create_array - function creates array and replaces intialised
  * pointer with specified char
  * @size: size of array
  * @c: char to replace
@@ -13,25 +13,25 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
-        unsigned int i = 0;
-    
-    if (size == 0)
-    {
-        printf("Unable to allocate memory\n");
-        return (NULL);
-    }
-    array = (char *)malloc(size * sizeof(char));
-    if (array == NULL)
-    {
-        printf("Array is  empty\n");
-        return (NULL);
-    }
-    else
-    {
-        for (i = 0; i < size; i++)
-        {
-            array[i] = c;
-        }
-    }
-    return (array);
+	unsigned int i;
+
+	if (size == 0)
+	{
+		printf("Unable to allocate memory\n");
+		return (NULL);
+	}
+	array = (char *)malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		printf("Array is  empty\n");
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			array[i] = c;
+		}
+	}
+	return (array);
 }
