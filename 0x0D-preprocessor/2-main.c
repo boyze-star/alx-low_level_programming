b39eff7728587036f21fe291e682598998406b9c
Thlakudi <thlakudimsibi760@gmail.com>
2023-06-19 14:55:02 -0700

    print out FILE name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function prints file name it was compliled from
+ *
+ * Return: name of file
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
