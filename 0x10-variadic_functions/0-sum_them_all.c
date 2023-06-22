39b5802653330d38d937c76c8699913abef88efd
Thlakudi <thlakudimsibi760@gmail.com>
2023-06-22 07:26:54 -0700

    return sum of parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,8 +3,9 @@
 /**
  * sum_them_all - function returns the sum of all its parameters
  * @n: number of total arguements
+ * @...: variable num of parameters
  *
- * Retrun: Always 0(Success), 1(Error)
+ * Return: If (n == 0) ,return 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -12,18 +13,20 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	va_list args;
-	va_start(args, n);
 
+	va_start(args, n);
 
 	if (n == 0)
 	{
 		return (0);
 	}
+
 	for (i = 0; i < n; i++)
 	{
 		value = va_arg(args, int);
 		sum += value;
 	}
 	va_end(args);
+
 	return (sum);
 }
