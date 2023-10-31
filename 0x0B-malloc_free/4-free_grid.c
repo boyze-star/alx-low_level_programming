11b8eb90989500ac430fd8b6481224f838afd982
Thlakudi <thlakudimsibi760@gmail.com>
2023-10-31 02:29:54 -0700

    free grid memory

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
  * free_grid -  frees a 2 dimensional grid
  * previously created by your alloc_grid
  * @grid: previously created grid
- * height: grid height
+ * @height: grid height
  *
  * Return: memory freed
  */
