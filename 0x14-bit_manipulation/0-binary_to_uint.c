e5c99bf377325abfca0dc94ab1f793a28161cbfc
Thlakudi <thlakudimsibi760@gmail.com>
2023-11-24 03:24:55 -0800

    converts binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,29 @@
 #include "main.h"
 
-/*
- * binary_to_uint - function that converts a binary number to an unsigned int
+/**
+ * binary_to_uint -converts a binary number to an unsigned int.
  * @b: pointing to a string of 0 and 1 chars
  *
- * Return: the converted number, or 0 if
- * there is one or more chars in the string b that is not 0 or 1
- * b is NULL
+ * Return:converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int total, power;
-	int len = 0;
+	int i;
+	unsigned int j;
 
-	if (b == NULL)
-	{
+	j = 0;
+	if (!b)
 		return (0);
-	}
-	for (len = 0; b[len]; len++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[len] != 0 && b[len] != 1)
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
 	}
-	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[len] == '1')
-		{
-			total += power;
-		}
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
 	}
-	return (total);
+	return (j);
 }
